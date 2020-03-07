03b522ad5e4ac39e142237a1d39d4932da73b43e
Papermo <zhi_mo@foxmail.com>
2020-03-07 11:04:47 +0800

    JSON完整版NC更新！！！

--- js/nowchat.js
+++ js/nowchat.js
@@ -1,35 +1,39 @@
 var $$ = mdui.JQ;
-var NC_version = "1.4"
-var NC_bulid = 200306
-var NC_channel = "NowChat_Public"
-    //Goeasy controler
-var userName, userLocation, userAvatar
-var goEasy = new GoEasy({
-    host: 'hangzhou.goeasy.io',
-    appkey: "BC-de02bc0bb7e54c009f43510d74c88156",
-    forceTLS: true,
-    onDisconnected: function() {
-        console.log('goeasy:连接断开！')
-        mdui.dialog({
-            title: '错误！',
-            content: '与远程服务器连接断开，请刷新后重试！',
-            modal: true,
-            closeOnEsc: false,
-            destroyOnClosed: true
-        });
-    },
-    onConnectFailed: function(error) {
-        console.log('goeasy:连接失败或错误！')
-        mdui.dialog({
-            title: '错误！',
-            content: '与远程服务器连接断开，请刷新后重试！',
-            modal: true,
-            closeOnEsc: false,
-            destroyOnClosed: true
-        });
-    }
-});
+//系统设置
+var NC_version = "1.4";
+var NC_bulid = 200306;
+var NC_channel = "NowChat_Public";
 var login_count = 0;
+//Goeasy controler
+var user_Name,
+    user_Location,
+    user_Avatar,
+    user_UUID,
+    goEasy = new GoEasy({
+        host: 'hangzhou.goeasy.io',
+        appkey: "BC-de02bc0bb7e54c009f43510d74c88156",
+        forceTLS: true,
+        onDisconnected: function() {
+            console.log('goeasy:连接断开！')
+            mdui.dialog({
+                title: '错误！',
+                content: '与远程服务器连接断开，请刷新后重试！',
+                modal: true,
+                closeOnEsc: false,
+                destroyOnClosed: true
+            });
+        },
+        onConnectFailed: function(error) {
+            console.log('goeasy:连接失败或错误！')
+            mdui.dialog({
+                title: '错误！',
+                content: '与远程服务器连接断开，请刷新后重试！',
+                modal: true,
+                closeOnEsc: false,
+                destroyOnClosed: true
+            });
+        }
+    });
 
 function randomNum(minNum, maxNum) {
     //随机整数生成器
@@ -56,40 +60,34 @@ function DIVinner(html, div = "textarea") {
 };
 
 function sendMessage() {
-    //发送标准对话框信息
     var msg = document.getElementById("messageBox").value
     if (msg !== "") {
-        goEasy.publish({
-            channel: NC_channel,
-            message: "<div class='mdui-row'><div class='mdui-col-xs-1'><img class='mdui-chip-icon mdui-float-right' src='" +
-                userAvatar + "'/></div><div class='mdui-col-xs-10'><div class='mdui-text-color-black-icon'>" +
-                userName + "</div><div class='mdui-chip mdui-color-white mdui-shadow-2'><span class='mdui-chip-title mdui-text-truncate' style='max-width:500px;'>" +
-                msg + "</span></div><br/><br/></div></div>"
-        });
+        pushMessage("markdown", msg)
         document.getElementById("messageBox").value = "";
     }
 };
 
-function commonTipMessage(msg) {
-    //发送公共系统信息
-    goEasy.publish({
-        channel: NC_channel,
-        message: "<br/><div style='text-align:center;' class='mdui-text-color-black-secondary'>" + msg + "</div><br/>"
-    });
-    document.getElementById('messageBox').value = "";
+function msg_processer(json) {
+    //处理JSON信息
+    if (NC_bulid < json.build) {
+        console.log("你的build号：" + NC_bulid + "与目前收到的build号" + json.build + "不匹配，请检查Github上的更新！")
+    };
+    if (json.type == "markdown") {
+        DIVinner("<div class='mdui-row'><div class='mdui-col-xs-1'><img class='mdui-chip-icon mdui-float-right' src='" +
+            json.userAvatar + "'/></div><div class='mdui-col-xs-10'><div class='mdui-text-color-black-icon'>" +
+            json.userName + "</div><div class='mdui-chip mdui-color-white mdui-shadow-2'><span class='mdui-chip-title mdui-text-truncate' style='max-width:500px;'>" +
+            json.message + "</span></div><br/><br/></div></div>")
+    };
+    if (json.type == "system") {
+        if (json.uuid == user_UUID) return;
+        DIVinner("<br/><div style='text-align:center;' class='mdui-text-color-black-secondary'>" +
+            json.message + "</div><br/>")
+    };
+
 };
 
-//pushMessage("markdown", "paper", "none", "test")
-
-function pushMessage(type, userName, userAvatar, msg, channel = NC_channel) {
-    //向Goeasy推送JSON数据
-    function pushOrigin(json) {
-        goEasy.publish({
-            channel: channel,
-            message: JSON.stringify(json)
-        });
-    }
-    uuid = $$.guid(userName);
+function pushMessage(type, msg, userName = user_Name, uuid = user_UUID, userAvatar = user_Avatar, channel = NC_channel) {
+    //将准备发送的信息打包成JSON,并向Goeasy推送
     if (type == "markdown") {
         var json = {
             "type": type,
@@ -101,8 +99,6 @@ function pushMessage(type, userName, userAvatar, msg, channel = NC_channel) {
             "build": NC_bulid,
             "channel": channel
         }
-        pushOrigin(json)
-        document.getElementById('messageBox').value = "";
     };
     if (type == "system") {
         var json = {
@@ -115,83 +111,50 @@ function pushMessage(type, userName, userAvatar, msg, channel = NC_channel) {
             "build": NC_bulid,
             "channel": channel
         }
-        pushOrigin(json)
     };
     if (type == "private") {
         DIVinner("<br/><div style='text-align:center;' class='mdui-text-color-black-secondary'>" + msg + "</div><br/>")
-    }
-};
-
-function pullMessage(userName, channel = NC_channel) {
-    //从Goeasy拉取JSON数据
-    function msg_processer(json) {
-        if (NC_bulid < json.build) {
-            console.log("你的build号：" + NC_bulid + "与目前收到的build号" + json.build + "不匹配，请检查Github上的更新！")
-        };
-        if (json.userName == userName) return;
-        if (json.type == "markdown") {
-            DIVinner("<div class='mdui-row'><div class='mdui-col-xs-1'><img class='mdui-chip-icon mdui-float-right' src='" +
-                json.userAvatar + "'/></div><div class='mdui-col-xs-10'><div class='mdui-text-color-black-icon'>" +
-                json.userName + "</div><div class='mdui-chip mdui-color-white mdui-shadow-2'><span class='mdui-chip-title mdui-text-truncate' style='max-width:500px;'>" +
-                json.message + "</span></div><br/><br/></div></div>")
-        };
-        if (json.type == "system") {
-            DIVinner("<br/><div style='text-align:center;' class='mdui-text-color-black-secondary'>" +
-                json.message + "</div><br/>")
-        };
-
+        return
     };
-    goEasy.subscribe({
-        channel: channel,
-        onMessage: function(message) {
-            msg_processer(JSON.parse(message.content));
-            var pulledMsg = JSON.parse(message.content);
-        }
+    msg_processer(json);
+    goEasy.publish({
+        channel: json.channel,
+        message: JSON.stringify(json)
     });
 };
 
-function privateTipMessage(msg) {
-    //发送私有系统信息
-    DIVinner("<br/><div style='text-align:center;' class='mdui-text-color-black-secondary'>" + msg + "</div><br/>")
+function pullMessage(uuid = user_UUID, channel = NC_channel) {
+    //从Goeasy拉取JSON数据
+    goEasy.subscribe({
+        channel: channel,
+        onMessage: function(message) {
+            var json = JSON.parse(message.content);
+            if (json.uuid == user_UUID) return;
+            msg_processer(json);
+        }
+    });
 };
 
 function logout() {
     //取消Goeasy频道监听
-    commonTipMessage("-- 用户 " + userName + " 已退出本聊天室 --");
+    pushMessage("system", "-- 用户 " + user_Name + " 已退出本聊天室 --");
     goEasy.unsubscribe({
         channel: NC_channel
     });
-    privateTipMessage("-- 您已退出聊天室，您将不会再接收到消息 --");
+    pushMessage("private", "-- 您已退出聊天室，您将不会再接收到消息 --")
 };
 
 function login() {
     //打开Goeasy频道监听
-    commonTipMessage("-- 用户 " + userName + " 已加入本聊天室 --");
-    goEasy.subscribe({
-            channel: NC_channel,
-            onMessage: function(message) {
-                DIVinner(message.content);
-            }
-        }
-
-    );
-    privateTipMessage("-- 您已加入聊天室：Public --");
+    pushMessage("system", "-- 用户 " + user_Name + " 已加入本聊天室 --");
+    pullMessage()
+    pushMessage("private", "-- 您已加入聊天室：Public --")
 };
 
 function refresh() {
     //刷新，其实没卵用。。。
-    goEasy.unsubscribe({
-        channel: NC_channel
-    });
-    goEasy.subscribe({
-            channel: NC_channel,
-            onMessage: function(message) {
-                DIVinner(message.content);
-            }
-        }
-
-    );
-    privateTipMessage("-- 已刷新 --");
+    pullMessage()
+    pushMessage("private", "-- 已刷新 --")
 };
 
 function openHAS() {
@@ -214,7 +177,10 @@ function init() {
     if (login_count !== 1) { return "none" };
 
     //随机为用户选取头像
-    userAvatar = "images/Avatar-" + randomNum(1, 10) + ".png";
+    user_Avatar = "images/Avatar-" + randomNum(1, 10) + ".png";
+
+    //生成用户UUID
+    user_UUID = $$.guid(user_Name);
 
     //启动enter键监听
     document.onkeydown = function(event) {
@@ -243,15 +209,17 @@ function init() {
     login();
 };
 
-//获取用户名等相关配置
-openHAS()
-var dialog_HAS = document.getElementById('HAS');
-dialog_HAS.addEventListener('confirm.mdui.dialog', function() {
-    var preg = /^[\u4E00-\u9FA5\uF900-\uFA2D|\w]{2,20}$/
-    userName = document.getElementById("HAS_UNI").value
-    if (preg.test(userName) == true) {
-        init(); //网页初始化
-    } else {
-        openHAS();
-    }
-});
+//获取用户名等相关配置,并启动。
+$$(function() {
+    openHAS()
+    var dialog_HAS = document.getElementById('HAS');
+    dialog_HAS.addEventListener('confirm.mdui.dialog', function() {
+        var preg = /^[\u4E00-\u9FA5\uF900-\uFA2D|\w]{2,20}$/
+        user_Name = document.getElementById("HAS_UNI").value
+        if (preg.test(user_Name) == true) {
+            init(); //网页初始化
+        } else {
+            openHAS();
+        }
+    });
+})
